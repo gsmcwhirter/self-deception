1376ecccbd104a7ecfd30b9ddbb8ee474b2bd180
Gregory McWhirter <greg@hallofkvasir.org>
2013-06-03 13:12:37 -0700

    Fixing checkArguments issue

--- stats_parser.js
+++ stats_parser.js
@@ -10,94 +10,92 @@ var args = require("optimist")
 
 require("buffertools") //monkey-patch
 
-  
-function checkArguments(argv){
-  if (!fs.statSync(argv.dir).isDirectory()){
-    throw new Error("dir argument was not a directory.");
-  }
+if (!fs.statSync(args.dir).isDirectory()){
+  console.log("Error: dir is not a directory.");
 }
-
-fs.readdir(args.dir, function (err, files){
-  if (err){
-    console.log("Error reading the output directory.");
-    return;
-  }
-  
-  var numFiles = files.length;
-  var doneFiles = 0;
-  console.log("Opened the output directory.");
-  
-  files.forEach(function (file){
-    console.log("Parsing %s...", file);
-    var rstream = fs.createReadStream(file)
-    
-      , end_state_started = false
-      , start_byte = 0
-      , incomplete_line = ""
-    
-      , players = []
-      , current_player = -1
-      ;
-    
-    rstream.on("readable", function (){
-      var buffer = rstream.read();
-      //TODO: parse output
-      
-      if (end_state_started){
-        start_byte = 0;
-      }
-      else if (buffer.toString().match(/^done.\s\n/g).length > 0) {
-        end_state_started = true;
-        start_byte = buffer.indexOf("done.\n");
-      }
-      
-      if (end_state_started){
-        var lines = buffer.toString('utf8', start_byte).split("\n");
-        lines[0] = incomplete_line + lines[0];
-        incomplete_line = lines.pop();
-        
-        lines.forEach(function (line){
-          var matches;
-          matches = line.match(/^\sPlayer (\d+)\s$/);
-          if (matches.length){
-            current_player = parseInt(matches[1], 10);
-            players[current_player] = {counts: [], proportions: []};
-            return;
-          }
-          
-          matches = line.match(/^Counts: (.+)$/);
-          if (matches.length){
-            players[current_player].counts.push(matches[1].trim()
-                                                          .split(/\s+/)
-                                                          .map(function (prop){
-                                                            return parseFloat(prop.trim());
-                                                          }));
-          }
-          
-          matches = line.match(/^\sProportions: (.+)$/);
-          if (matches.length){
-            players[current_player].proportions.push(matches[1].trim()
-                                                               .split(/\s+/)
-                                                               .map(function (prop){
-                                                                 return parseFloat(prop.trim());
-                                                               }));
-          }
-        });
-      }
-    }); 
-    
-    rstream.on("end", function (){
-      doneFiles += 1;
-      
-      console.log(players);
-      
-      whenDone();
-    });
-  });
-  
-  function whenDone(){
-    if (doneFiles >= numFiles){
-      //TODO: output stats
+else {
+  fs.readdir(args.dir, function (err, files){
+    if (err){
+      console.log("Error reading the output directory.");
+      return;
     }
-  }
-});
+    
+    var numFiles = files.length;
+    var doneFiles = 0;
+    console.log("Opened the output directory.");
+    
+    files.forEach(function (file){
+      console.log("Parsing %s...", file);
+      var rstream = fs.createReadStream(file)
+      
+        , end_state_started = false
+        , start_byte = 0
+        , incomplete_line = ""
+      
+        , players = []
+        , current_player = -1
+        ;
+      
+      rstream.on("readable", function (){
+        var buffer = rstream.read();
+        //TODO: parse output
+        
+        if (end_state_started){
+          start_byte = 0;
+        }
+        else if (buffer.toString().match(/^done.\s\n/g).length > 0) {
+          end_state_started = true;
+          start_byte = buffer.indexOf("done.\n");
+        }
+        
+        if (end_state_started){
+          var lines = buffer.toString('utf8', start_byte).split("\n");
+          lines[0] = incomplete_line + lines[0];
+          incomplete_line = lines.pop();
+          
+          lines.forEach(function (line){
+            var matches;
+            matches = line.match(/^\sPlayer (\d+)\s$/);
+            if (matches.length){
+              current_player = parseInt(matches[1], 10);
+              players[current_player] = {counts: [], proportions: []};
+              return;
+            }
+            
+            matches = line.match(/^Counts: (.+)$/);
+            if (matches.length){
+              players[current_player].counts.push(matches[1].trim()
+                                                            .split(/\s+/)
+                                                            .map(function (prop){
+                                                              return parseFloat(prop.trim());
+                                                            }));
+            }
+            
+            matches = line.match(/^\sProportions: (.+)$/);
+            if (matches.length){
+              players[current_player].proportions.push(matches[1].trim()
+                                                                 .split(/\s+/)
+                                                                 .map(function (prop){
+                                                                   return parseFloat(prop.trim());
+                                                                 }));
+            }
+          });
+        }
+      }); 
+      
+      rstream.on("end", function (){
+        doneFiles += 1;
+        
+        console.log(players);
+        
+        whenDone();
+      });
+    });
+    
+    function whenDone(){
+      if (doneFiles >= numFiles){
+        //TODO: output stats
+      }
+    }
+  });
+}
